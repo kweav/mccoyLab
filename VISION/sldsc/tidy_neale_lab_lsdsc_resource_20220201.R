dca5774c6c6a9f01b1e6bdbbb03ce8de205b0d48
Kate Weaver <kweave23@jhu.edu>
2022-02-11 16:58:38 -0500

    get just wgets and descriptions for irnt

---
+++ VISION/sldsc/tidy_neale_lab_lsdsc_resource_20220201.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+nl_df <- read_csv("neale_lab_lsdsc_resource_20220201.csv")
+nl_df <- nl_df %>% 
+  separate(phenotype, into=c("phenotypeDescA", "phenotypeDescB", "phenotypeDescC", "phenotypeDescD", "normalized"), sep="_", fill="left", remove=FALSE) %>% 
+  dplyr::filter(normalized == "irnt" & sex != "both_sexes")
+  
+description_df <- nl_df %>% 
+  dplyr::select(phenotype, description, sex)
+wget_df <- nl_df %>% 
+  dplyr::select(ldsc_sumstat_wget)
+
+write_csv(description_df, "nl_sumstat_descriptions.csv")
+write.table(wget_df, "nl_sumstat_wgets.csv", col.names=FALSE, row.names=FALSE, quote=FALSE)
