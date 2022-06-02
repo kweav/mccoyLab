4a455a06e8b9bd6161726b357368f948bf6a4a24
Kate Weaver <kweave23@jhu.edu>
2022-06-02 15:58:58 -0400

    update with batch3

--- collabs/doubleseq_2021/code/tidy_data/make_tidied_data.R
+++ collabs/doubleseq_2021/code/tidy_data/make_tidied_data.R
@@ -1,22 +1,38 @@
 library(tidyverse)
 library(data.table)
+library(naniar)
 
-meta_raw1 <- "raw_meta/DoubleSeq_copied_20210408_kw.csv"
-meta_raw2 <- "raw_meta/DoubleSeqBatch2_20211206.csv"
+meta_raw1 <- "raw_meta/DoubleSeqBatch1_20220531.csv"
+meta_raw2 <- "raw_meta/DoubleSeqBatch2_20220531.csv"
+meta_raw3 <- "raw_meta/DoubleSeqBatch3_20220531.csv"
 
-tidy_raw_metadata <- function(meta_file){ 
-  comb_col_names <- c("Study_Participant_ID", "Embryo_ID", "AOD", "GC", "Oocyte_Age",
+tidy_raw_metadata <- function(meta_file, batch){ 
+  if (batch == 1 | batch == 2){
+    comb_col_names <- c("Study_Participant_ID", "Embryo_ID", "AOD", "GC", "Oocyte_Age",
                       "Uterus_Age", "Sperm_Age", "BMI", "Infertility_type",
                       "Infertility_diagnosis", "Previous_pregnancy","Past_med_hist",
                       "Past_surgical_hist", "FSH_D3", "AMH", "AFC", "LH", "E2_Day_2_3",
                       "Type_of_trigger", "Total_Gn", "E2_on_trigger", "Num_eggs",
-                      "Num_MII", "Num_2PN", "Num_Blasts", "TMC", "Time_to_OPU_hr",
+                      "Num_MII", "Num_2PN", "Num_Blasts", "Embryoscope", "TMC", "Time_to_OPU_hr",
                       "Time_to_stripping_hr", "Time_to_ICSI_hr", "Time_to_Biopsy_hr",
                       "Embryo_grade_at_freezing", "Interpretation", "Natural_cycle",
                       "Medication", "lining_thickness_mm", "Prednisone", "Fragmin",
                       "EG", "HCG", "IL", "GCSF", "Metformin","PRP","Sildenafil",
                       "Transferring_physician", "Transfer_catheter_used", "Pregnant", "Ongoing_pregnancy",
                       "Final_outcome", "cDNA_RT_Date", "Library_Prep_Date", "Sequencing_Date")
+  } else {
+    comb_col_names <- c("Study_Participant_ID", "Embryo_ID", "AOD", "GC", "Oocyte_Age",
+                      "Uterus_Age", "Sperm_Age", "BMI", "Infertility_type",
+                      "Infertility_diagnosis", "Previous_pregnancy","Past_med_hist",
+                      "Past_surgical_hist", "FSH_D3", "AMH", "AFC", "LH", "E2_Day_2_3",
+                      "Type_of_trigger", "Total_Gn", "E2_on_trigger", "Num_eggs",
+                      "Num_MII", "Num_2PN", "Num_Blasts", "Embryoscope", "TMC",
+                      "Embryo_grade_at_freezing", "Interpretation", "Natural_cycle",
+                      "Medication", "lining_thickness_mm", "Prednisone", "Fragmin",
+                      "EG", "HCG", "IL", "GCSF", "Metformin","PRP","Sildenafil",
+                      "Transferring_physician", "Transfer_catheter_used", "Pregnant", "Ongoing_pregnancy",
+                      "Final_outcome", "cDNA_RT_Date", "Library_Prep_Date", "Sequencing_Date")
+  }
   
   meta <- read.csv(meta_file, header = FALSE, skip=1, col.names = comb_col_names, row.names = 2) %>% 
     as.data.frame %>% 
@@ -24,15 +40,15 @@ tidy_raw_metadata <- function(meta_file){
     na_if("TBD") %>% 
     mutate(AMH = as.numeric(as.character(recode(AMH, ">127"="128")))) %>%
     mutate(LH = as.numeric(as.character(recode(as.character(LH), ">9"="10")))) %>%
-    mutate(E2_Day_2_3 = as.numeric(as.character(recode(E2_Day_2_3, "<18"="17")))) %>%
-    mutate(Embryo_grade_at_freezing = recode(Embryo_grade_at_freezing, "1AA"="Good", "1AB"="Good", "1BA"="Fair", "1BB"="Fair", "1CB"="Poor", "2AA"="Good", "2AB"="Good", "2BA"="Fair", "2BB"="Fair", "2CC"="Poor", "3AA"="Good", "3BB"="Fair")) %>%
-    mutate(Interpretation = recode(Interpretation, "EUPLOID"=0, "MOSAIC"=1)) %>%
+    mutate(E2_Day_2_3 = as.numeric(as.character(recode(as.character(E2_Day_2_3), "<18"="17")))) %>%
+    mutate(Embryo_grade_at_freezing = recode(Embryo_grade_at_freezing, "1AA"="Good", "1AB"="Good", "1BA"="Fair", "1BB"="Fair", "1CB"="Poor", "2AA"="Good", "2AB"="Good", "2BA"="Fair", "2BB"="Fair", "2BC"="Fair" ,"2CC"="Poor", "3AA"="Good", "3BB"="Fair")) %>%
+    mutate(Interpretation = recode(Interpretation, "EUPLOID"=0, "MOSAIC"=1, "MOSIAC"=1)) %>%
     mutate(Prednisone = as.factor(recode(as.character(Prednisone), "B"="1"))) %>%
     mutate(Transferring_physician = as.factor(recode(as.character(Transferring_physician), "PS" = "1", "AB" = "2", "KG" = "3", "CL"="4", "Fellow"="5", "AK"="6"))) %>%
     mutate(Transfer_catheter_used = as.factor(recode(as.character(Transfer_catheter_used), "TomCat"="1", "Tomcat"="1", "TOMCAT"="1", "Wallace"="2", "Cook"="3"))) %>%
-    mutate(cDNA_RT_Date = recode(cDNA_RT_Date,  "19-Mar-20"="1", "13-Apr-20"="2", "17-Jul-20"="3", "12-Aug-20"="4", "18-Aug-20"="5", "25-Aug-20"="6", "2-Sep-20"="7", "8-Sep-20"="8", "23-Sep-20"="9", "16-Oct-20"="10", "27-Oct-20"="11", "5-Nov-20"="12", "9-Dec-20"="13", "14-Jan-21"="14", "22-Feb-21"="15", "26-Mar-21"="16", "5-May-21"="17", "27-May-21"="18", "22-Jun-21"="19", "20-Jul-21"="20")) %>%
-    mutate(Library_Prep_Date = recode(Library_Prep_Date, "11-Dec-20"="0", "11-Feb-21"="1", "12-Aug-21"="2")) %>%
-    mutate(Sequencing_Date = recode(Sequencing_Date, "14-Dec-20"= "0", "5-Mar-21"="1", "28-Sep-21"="2")) %>%
+    mutate(cDNA_RT_Date = recode(cDNA_RT_Date,  "19-Mar-20"="1", "13-Apr-20"="2", "17-Jul-20"="3", "12-Aug-20"="4", "18-Aug-20"="5", "25-Aug-20"="6", "2-Sep-20"="7", "8-Sep-20"="8", "23-Sep-20"="9", "16-Oct-20"="10", "27-Oct-20"="11", "5-Nov-20"="12", "9-Dec-20"="13", "14-Jan-21"="14", "22-Feb-21"="15", "26-Mar-21"="16", "5-May-21"="17", "27-May-21"="18", "22-Jun-21"="19", "20-Jul-21"="20", "2-Sep-21"="21", "28-Sep-21" = "22", "4-Nov-21"="23", "22-Jan-22"="24")) %>%
+    mutate(Library_Prep_Date = recode(Library_Prep_Date, "11-Dec-20"="0", "11-Feb-21"="1", "12-Aug-21"="2", "1-Mar-22"="3")) %>%
+    mutate(Sequencing_Date = recode(Sequencing_Date, "14-Dec-20"= "0", "5-Mar-21"="1", "28-Sep-21"="2", "15-Mar-22"="3")) %>%
     mutate(across(c("AOD", "GC", "Infertility_type", "Previous_pregnancy", "Past_surgical_hist", "Pregnant", "Ongoing_pregnancy", "Final_outcome", "Embryo_grade_at_freezing", "Interpretation", "cDNA_RT_Date", "Library_Prep_Date", "Sequencing_Date"), as.factor))
   
   
@@ -66,18 +82,23 @@ tidy_raw_metadata <- function(meta_file){
   meta <- separate(meta, col="NInfD", into=c("InfD_SSM_GC", "InfD_Egg_factor", "InfD_MF", "InfD_Uterine_factor", "InfD_TF", "InfD_RPL", "InfD_RIF", "InfD_Unexplained"), sep='_') %>% 
     separate(col="NPMdH", into=c("PMdH_none", "PMdH_vasculitis", "PMdH_immune", "PMdH_stress_hormones"), sep="_") %>% 
     mutate(across(c("InfD_SSM_GC", "InfD_Egg_factor", "InfD_MF", "InfD_Uterine_factor", "InfD_TF", "InfD_RPL", "InfD_RIF", "InfD_Unexplained", "PMdH_none", "PMdH_vasculitis", "PMdH_immune", "PMdH_stress_hormones"), as.factor)) %>% 
-    within(rm("Infertility_diagnosis", "Past_med_hist"))
+    within(rm("Infertility_diagnosis", "Past_med_hist")) %>% as.data.frame()
   
   return(meta)
 
 }
 
-meta_tidied1 <- tidy_raw_metadata(meta_raw1)
+meta_tidied1 <- tidy_raw_metadata(meta_raw1, 1)
 write.csv(meta_tidied1, "tidied_meta/tidied_meta_batch1_CREATE_kw.csv")
-meta_tidied2 <- tidy_raw_metadata(meta_raw2)
+meta_tidied2 <- tidy_raw_metadata(meta_raw2, 2)
 write.csv(meta_tidied2, "tidied_meta/tidied_meta_batch2_CREATE_kw.csv")
+meta_tidied3 <- tidy_raw_metadata(meta_raw3, 3)
+write.csv(meta_tidied3, "tidied_meta/tidied_meta_batch3_CREATE_kw.csv")
 
-meta_full <- rbind(meta_tidied1,  meta_tidied2)
+#meta_full <- rbind(meta_tidied1,  meta_tidied2)
+meta_full <- data.table::rbindlist(list(meta_tidied1, meta_tidied2, meta_tidied3), fill=TRUE) %>% `rownames<-`(c(rownames(meta_tidied1), rownames(meta_tidied2), rownames(meta_tidied3)))
 #write.csv(meta_full, "tidied_meta/tidied_meta_CREATE_kw_20211217.csv")
-write.csv(meta_full, "tidied_meta/tidied_meta_CREATE_kw_20220308.csv")
+#write.csv(meta_full, "tidied_meta/tidied_meta_CREATE_kw_20220308.csv")
+write.csv(meta_full, "tidied_meta/tidied_meta_CREATE_kw_20220531.csv")
 
+naniar::vis_miss(meta_full)
