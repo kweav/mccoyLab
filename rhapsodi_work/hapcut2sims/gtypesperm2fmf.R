cc089ff726117138bc89e35bf39c52f44c0c5295
Kate Weaver <kweave23@jhu.edu>
2022-10-03 15:19:12 -0400

    hash in column name now

--- rhapsodi_work/hapcut2sims/gtypesperm2fmf.R
+++ rhapsodi_work/hapcut2sims/gtypesperm2fmf.R
@@ -12,7 +12,7 @@ fread.tfile<-function(tfile){
 
 tofmf<-function(snptab,daconechr){
   # Converts GenotypeSperm output to .fmf-style data.table. Keeps track of observations of SNPs that aren't included (those that observe multiple alleles in one cell, observations of non-ref or alt allele)
-  # In: snptab, output of fread.tfile with columns CHROM, POS, ID, REF, ALT, ind
+  # In: snptab, output of fread.tfile with columns #CHROM, POS, ID, REF, ALT, ind
   #     daconechr, GenotypeSperm output for the one chromosome in snptab; only the following columns: "chr","pos","cell","A","C","G","T","N"
   # Out: list of: fmf, 2 column data.table. First column is cell, second is list of vectors. Each vector is fragment for a consecutive pair of SNPs from that cell. Vector format described in onefrag() comments.
   #               bads, data.table of relevant info of daconechr rows (SNP observations) that were excluded from FMF due to having multiple or the wrong alleles observed
@@ -20,7 +20,7 @@ tofmf<-function(snptab,daconechr){
   # Subfunctions
   idfails<-function(oneobs){
     # Identifies whether the input SNP observation fails the checks of 1) observing only one allele, 2) seeing only N bases, or 2) seeing a non-ref/non-alt allele
-    # In: oneobs, one row representing SNP observation to check. Columns (in order): CHROM, POS, ID, REF, ALT, ind, chr, cell, A, C, G, T, N
+    # In: oneobs, one row representing SNP observation to check. Columns (in order): #CHROM, POS, ID, REF, ALT, ind, chr, cell, A, C, G, T, N
     # Out: logical. TRUE if this observation fails either check; FALSE otherwise
 
     if(sum(unlist(oneobs[,.(A,C,G,T)])>0)>1){ # check multiple bases observed
@@ -89,15 +89,15 @@ tofmf<-function(snptab,daconechr){
     
   # Drop any SNP observations of multiple alleles, non-ref or alt alleles
   ckbads<-daconechr[,idfails(.SD),by=row.names(daconechr)]$V1 # need to retain as data.table. Unfortunately, this is quite slow. Takes 43 seconds for 10k observations
-  bads<-daconechr[ckbads,.(CHROM,POS,ID,REF,ALT,cell,A,C,G,T,N)]
+  bads<-daconechr[ckbads,.(`#CHROM`,POS,ID,REF,ALT,cell,A,C,G,T,N)]
   daconechr<-daconechr[!ckbads,]
-  cat(paste("SNP observations to exclude identified (chromoosome",daconechr[1,CHROM],")\n")) # for tracking
+  cat(paste("SNP observations to exclude identified (chromosome",daconechr[1,`#CHROM`],")\n")) # for tracking
 
   # Create FMFs on per-cell basis
   setkeyv(daconechr,c("cell","POS"))
   fmf<-daconechr[,list(list(onecell.frags(.SD,myn=10))),by=cell] # using a list column: vector will display with commas
   fmf<-fmf[sapply(fmf$V1,length)>0,] # get rid of nulls. Now each cell is a list of lists.
-  cat(paste("Initial FMF info generated (chromoosome",daconechr[1,CHROM],")\n")) # for tracking
+  cat(paste("Initial FMF info generated (chromosome",daconechr[1,`#CHROM`],")\n")) # for tracking
   
   # Return fmf, excludeds
   return(list(fmf,bads))
@@ -125,24 +125,24 @@ writefmf<-function(fmf,outfile){
 }
 
 #### Arguments and input ####
-args<-commandArgs(TRUE)
-if(length(args)!=4){
-  stop("Usage: Rscript gtypesperm2fmf.R
-       <1. Path to GATK VariantsToTable output with columns CHROM, POS, ID, REF, ALT. For one chromosome only. >
-       <2. Path to GenotypeSperm output - if this file ends in .gz, it will be read as a gzip (via linux; may not work on other platforms depending on zcat install) >
-       <3. Path to no-header list of cell barcodes to include in output >
-       <4. Output filestem (.fmf file written, as well as GenotypeSperm-format information for SNP observations that were excluded) >
-       ",call=F)
-}
+#args<-commandArgs(TRUE)
+#if(length(args)!=4){
+#  stop("Usage: Rscript gtypesperm2fmf.R
+#       <1. Path to GATK VariantsToTable output with columns CHROM, POS, ID, REF, ALT. For one chromosome only. >
+#       <2. Path to GenotypeSperm output - if this file ends in .gz, it will be read as a gzip (via linux; may not work on other platforms depending on zcat install) >
+#       <3. Path to no-header list of cell barcodes to include in output >
+#       <4. Output filestem (.fmf file written, as well as GenotypeSperm-format information for SNP observations that were excluded) >
+#       ",call=F)
+#}
 
-#tfile<-"~/mccoyLab/rhapsodi_work/hapcut2sims/variantinfo_g1000_s30000_cov_0.001_seqerr_0.005_avgr_1_rs42.vcf" #args[1]
-tfile <- args[1]
-#dacfile<-"~/mccoyLab/rhapsodi_work/hapcut2sims/gtypesperm_g1000_s30000_cov_0.001_seqerr_0.005_avgr_1_rs42.txt" #args[2]
-dacfile <- args[2]
-#bcfile<-"~/mccoyLab/rhapsodi_work/hapcut2sims/cellbarcodes_g1000_s30000_cov_0.001_seqerr_0.005_avgr_1_rs42.txt" #args[3]
-bcfile <- args[3]
-#outstem<-"~/mccoyLab/rhapsodi_work/hapcut2sims/g1000_s30000_cov_0.001_seqerr_0.005_avgr_1_rs42_" #args[4]
-outstem <- args[4]
+tfile<-"~/mccoyLab/rhapsodi_work/hapcut2sims/variantinfo_g1000_s30000_cov_0.001_seqerr_0.005_avgr_1_rs42.vcf" #args[1]
+#tfile <- args[1]
+dacfile<-"~/mccoyLab/rhapsodi_work/hapcut2sims/gtypesperm_g1000_s30000_cov_0.001_seqerr_0.005_avgr_1_rs42.txt" #args[2]
+#dacfile <- args[2]
+bcfile<-"~/mccoyLab/rhapsodi_work/hapcut2sims/cellbarcodes_g1000_s30000_cov_0.001_seqerr_0.005_avgr_1_rs42.txt" #args[3]
+#bcfile <- args[3]
+outstem<-"~/mccoyLab/rhapsodi_work/hapcut2sims/g1000_s30000_cov_0.001_seqerr_0.005_avgr_1_rs42_" #args[4]
+#outstem <- args[4]
 
 snptab.glob<-fread.tfile(tfile)
 if(substr(dacfile,nchar(dacfile)-1,nchar(dacfile))=="gz"){ # set up command to read gz
